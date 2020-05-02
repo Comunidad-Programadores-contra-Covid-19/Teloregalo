ab8fb05a14522ff0dbc039d4a82b9d844b18ee76
Melanie Salzman <salzmanmelanie@gmail.com>
2020-05-02 07:42:52 -0300

    Added specific offer to mp checkout

--- resources/views/stores/index_profile.blade.php
+++ resources/views/stores/index_profile.blade.php
@@ -1,25 +1,9 @@
 @extends('layouts.app')
-<?php
-// SDK de Mercado Pago
-require '../vendor/autoload.php';
 
-    \MercadoPago\SDK::setAccessToken("TEST-4473036575801903-042214-90e96cb43dab864cda0953e27f5c8539-553262681");
-    
-
-// Crea un objeto de preferencia
-$preference = new MercadoPago\Preference();
-
-// Crea un ítem en la preferencia
-$item = new MercadoPago\Item();
-$item->title = 'Mi producto';
-$item->quantity = 1;
-$item->unit_price = 75.56;
-$preference->items = array($item);
-$preference->save();
-
-?>
 
 @section('content')
+
+
 <body>
     <div class="container">
         <div class="row">
@@ -35,14 +19,9 @@ $preference->save();
                 <div class="card-body">
                 <h5 class="card-title">{{ $offer->name_offer }}</h5>
                   <p class="card-text">{{ $offer->description_offer }}</p>
-                  <p class="card-text">{{ $offer->cost }}</p>
-                  
-                  <form action="/" method="POST">
-                            <script
-                            src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
-                            data-preference-id="<?php echo $preference->id; ?>">
-                            </script>
-                            </form>
+                  <p class="card-text">$ {{ $offer->cost }}</p>
+
+                  <a href="{{ route('offers.show', $offer->id) }}" class="btn-principal">Regalar</a>
 
                   <a href="{{ route('otps.create') }}" class="btn btn-primary">Retirar</a>
                 </div>
