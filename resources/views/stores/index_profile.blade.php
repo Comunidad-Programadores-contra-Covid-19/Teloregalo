17736e60fa03da1eacfe4628cc7d14230ea3c262
Melanie Salzman <salzmanmelanie@gmail.com>
2020-05-01 22:03:02 -0300

    Added mp in right place and added description and cost in product

--- resources/views/stores/index_profile.blade.php
+++ resources/views/stores/index_profile.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
-<?php  ?>
+<?php
+// SDK de Mercado Pago
+require '../vendor/autoload.php';
+
+    \MercadoPago\SDK::setAccessToken("TEST-4473036575801903-042214-90e96cb43dab864cda0953e27f5c8539-553262681");
+    
+
+// Crea un objeto de preferencia
+$preference = new MercadoPago\Preference();
+
+// Crea un ítem en la preferencia
+$item = new MercadoPago\Item();
+$item->title = 'Mi producto';
+$item->quantity = 1;
+$item->unit_price = 75.56;
+$preference->items = array($item);
+$preference->save();
+
+?>
 
 @section('content')
 <body>
@@ -16,8 +34,16 @@
                 <img class="card-img-top" src="https://www.pngkit.com/png/full/47-477296_clip-freeuse-library-grocery-products-svg-png-icon.png" alt="Card image cap">
                 <div class="card-body">
                 <h5 class="card-title">{{ $offer->name_offer }}</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Comprar</a>
+                  <p class="card-text">{{ $offer->description_offer }}</p>
+                  <p class="card-text">{{ $offer->cost }}</p>
+                  
+                  <form action="/" method="POST">
+                            <script
+                            src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+                            data-preference-id="<?php echo $preference->id; ?>">
+                            </script>
+                            </form>
+
                   <a href="{{ route('otps.create') }}" class="btn btn-primary">Retirar</a>
                 </div>
               </div>
