7d12d6bc5f78c8f579be3e7e07750516a96c89d5
Luciano Kapluk <kaplukluciano@gmail.com>
2020-05-01 21:27:48 -0300

    add FAQ, donar and Spanish Errors

---
+++ resources/views/teloregalo/preguntasFrecuentes.blade.php
@@ -0,0 +1,91 @@
+@extends('layouts.app')
+@section('content')
+    
+    <div class="container">
+        <section id="contenedorPreguntas">
+
+            <img src="Regursos%20graficos/preguntas.svg" alt="">
+
+            <h1>Preguntas frecuentes</h1>
+
+            <h3>¿Cómo funciona?</h3>
+            <p>Los vecinos pueden comprar a través de la aplicación a un comercio de confianza.</p>
+            <p>Lo que compren lo podrá retirar cualquier persona que se encuentre trabajando en situación de riesgo contra el coronavirus.</p>
+
+            <h3>¿A quiénes están destinados los regalos?</h3>
+            <p>A cualquier trabajador esencial que se encuentre trabajando cuidándonos del Covid-19.</p>
+            <p>Ejemplo: personal de la salud, de seguridad, bomberos, personal de limpieza, choferes, etc.</p>
+
+            <h3>¿Cómo hago un regalo?</h3>
+            <p>Elegí un comercio de tu confianza que figure en la aplicación, seleccioná el producto que quieras regalar y aboná a través de Mercado Pago.</p>
+
+            <h3>¿Me entero cuando mi regalo fue entregado?</h3>
+            <p>Sí, recibirás un correo electrónico.</p>
+
+            <h3>¿Yo elijo a quién le doy mi regalo?</h3>
+            <p>No, solo elegís el comercio y el producto.</p>
+
+            <h3>¿Figuran los nombres de quienes regalamos?</h3>
+            <p>No. La compra del regalo es anónima.</p>
+            <p>Tampoco es revelado el nombre de la persona que retira el regalo.</p>
+
+            <h3>¿Cómo doy de alta mi comercio para recibir compras?</h3>
+            <p>Ingresando en la opción “Registrar comercio” debés completar el formulario de registro.</p>
+
+            <h3>¿Hay comisión?</h3>
+            <p>No.</p>
+
+            <h3>¿Cómo hace el comercio para validar que la persona que reclama el regalo es un trabajador esencial (Héroe)?</h3>
+            <p>Queda a total discreción del comerciante entregar los regalos a las personas que acrediten estar trabajando para ayudarnos a todos. Como sugerencia pueden pedir el permiso de circulación obligatoria.</p>
+
+            <h3>Soy personal esencial ¿Cómo retiro un regalo?</h3>
+            <p>Seleccioná la opción “Soy un héroe” y completá el formulario.</p>
+            <p>Elegís en el mapa el comercio y regalo a retirar. Luego se te enviará un código para presentar en el comercio.</p>
+
+            <h3>¿Cómo hace el Héroe para saber qué comercios tienen un regalo disponible?</h3>
+            <p>En el Mapa del Home se puede visualizar el stock de cada comercio.</p>
+
+            <h3>¿Para qué es la donación a la comunidad?</h3>
+            <p>Nuestros proyectos son de impacto social sin fines de lucro.</p>
+            <p>Los fondos recaudados se utilizan para mantener en pie los proyectos y re invertir en otros nuevos que ayuden a la sociedad.</p>
+            
+        </section>
+
+    </div>
+    <!-- Fin contenedor -->
+
+    <!-- Inicio Footer -->
+    <footer style="margin-top: 1.5em;">
+        <div id="contenedorFooter">
+            <div class="row">
+                <div class="col-lg-2" id="logoFooter">
+                    <a href="#" >
+                        teloregalo
+                        <img src="Regursos%20graficos/logo.svg" width="35" height="35" alt="">
+                    </a>
+                </div>
+                
+                <div class="col-lg-7" id="menuFooter">
+                    <ul>
+                        <li>
+                        <a href="#">Comercios adheridos</a>
+                        </li>
+                        <li>
+                        <a href="#">Preguntas frecuentes</a>
+                        </li>
+                        <li>
+                            <a href="#">Quiénes somos</a>
+                            </li>
+                        <li>
+                            <a href="#">Donar</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3" id="año">
+                    <p>2020 Te lo regalo</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+@endsection
